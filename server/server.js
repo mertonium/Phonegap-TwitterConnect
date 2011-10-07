7b06753283a4ac509d7e71a93fd9af868a829241
John Mertens <john.mertens@gmail.com>
2011-10-07 11:10:14 -0700

    Lookup admin cleartext via couch. Add couchapp.

--- server/server.js
+++ server/server.js
@@ -1,40 +1,69 @@
-var express = require('express');
+var express = require('express'),
+    cradle = require('cradle');
 
+var config = {
+  couchhost: 'http://localhost',
+  couchport: '5984',
+  auth: {
+    username: 'admin',
+    password: 'admin'
+  }
+};
+
+var couch = new(cradle.Connection)({ host: config.couchhost, port: config.couchport, auth: config.auth});
 var app = express.createServer();
 
 app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'idonttoleratewheat'}));
   app.use(app.router);
 });
 
 app.configure('development', function(){
-  app.use(express.static(__dirname + '/public'));
+  //app.use(express.static(__dirname + '/public'));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
   var oneYear = 31557600000;
-  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  //app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
   app.use(express.errorHandler());
 });
 
-
-app.get('/', function(req, res){
-  res.redirect('/index.html', 301);
+app.get('/', function(req, res) {
+  res.send('Nothing to see here.');
 });
 
-app.get('/nextrip/:stopid', function(req, res){
-  nextrip.getNextTrip(req.params.stopid, {}, function(departures) {
-    res.send(departures);
-  });
-});
-
-app.get('/my511/:agency/:stopid', function(req, res){
-  var prefix = my511.getPrefix(req.params.agency);
-  var stop_id = prefix.toString() + req.params.stopid.toString();
-  my511.getNextTrip(stop_id, { }, function(departures) {
-    res.send(departures);
+app.post('/auth', function(req, res){
+  
+  var twitterid = req.body.twitterid;
+  var authtoken = req.body.authtoken;
+  var username = req.body.username;
+  
+  console.log('twitterid = '+ twitterid);
+  console.log('authtoken = '+ authtoken);
+  
+  // TODO: Insert the verification with Twitter (or more oauth providers) here
+  
+  // Get the user's password from the couch
+  couch.database('_users').list('users/arrayvalues/usernames', 
+    { key : username }, 
+    function(err, doc) {
+      var respObj = {};
+      
+      if(err) {
+        res.send(err, 404);
+      } else {
+        if(doc.length) {
+          console.log(doc);
+          respObj.cleartext = doc[0];
+          res.send(respObj);
+        } else {
+          res.send(username+' does not exist.', 404);
+        }
+      }
   });
 });
 
